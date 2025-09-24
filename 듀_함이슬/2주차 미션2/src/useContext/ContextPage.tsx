92f532d13b58598024b43063fe6c9fa2ec3fb8db
Dew <eidnwq@gmail.com>
2025-09-24 22:50:20 +0900

    :recycle: Refactor: 최상위 div 제거

--- 듀_함이슬/2주차 미션2/src/useContext/ContextPage.tsx
+++ 듀_함이슬/2주차 미션2/src/useContext/ContextPage.tsx
@@ -3,18 +3,16 @@ import Navbar from "./Navbar";
 import ThemeContent from "./ThemeContent";
 
 export default function ContextPage() {
-    
-    
+
+
   return (
-    <div>
-        <ThemeProvider>
-            <div className="flex flex-col items-center justify-center min-h-screen">
-                <Navbar />
-                <main className="flex-1 w-full">
-                    <ThemeContent />
-                </main>
-            </div>
-        </ThemeProvider>
-    </div>
+    <ThemeProvider>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <Navbar />
+        <main className="flex-1 w-full">
+          <ThemeContent />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
