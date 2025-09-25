e309fa0b19973b1f5774d31bfbcfc6202cacaaee
Kim YeWon <ymh20804@naver.com>
2025-09-25 09:24:30 +0900

    ✨ Feat: 미션02_다크모드 구현 추가

--- 도토_김예원/2주차 미션_02/src/components/ContextPage.tsx
+++ 도토_김예원/2주차 미션_02/src/components/ContextPage.tsx
@@ -5,6 +5,7 @@ import ThemeContent from "./ThemeContent";
 export default function ContextPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
+            {/* 우산으로 덮어주기 */}
             <ThemeProvider>
                 <Navbar/>
                 <main className="flex-1 w-full">
