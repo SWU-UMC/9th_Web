66d4d868094248a7d6862df78813b08438f6ecc9
Dew <eidnwq@gmail.com>
2025-09-29 20:34:13 +0900

    :fire: Remove: 사용되지 않는 타입 제거

--- 듀_함이슬/3주차 미션/2-3/src/types/movie.ts
+++ 듀_함이슬/3주차 미션/2-3/src/types/movie.ts
@@ -23,53 +23,14 @@ export type MovieResponse = {
 };
 
 export type MovieDetail = {
-    adult: boolean,
     backdrop_path: string;
-    belongs_to_collection: null;
-    budget: number;
-
-    genres: {
-        id: number; name: string
-    }[];
-
-    homepage: string;
     id: number;
-    imdb_id: string | null;
-    origin_country: string[];
-    original_language: string;
-    original_title: string;
     overview: string;
-    popularity: number;
-    poster_path: string | null;
-
-    production_companies: {
-        id: number;
-        logo_path: string | null;
-        name: string;
-        origin_country: string;
-    }[];
-
-    production_countries: {
-        iso_3166_1: string;
-        name: string;
-    }[];
-
     release_date: string;
-    revenue: number;
     runtime: number;
-
-    spoken_languages: {
-        english_name: string;
-        iso_639_1: string;
-        name: string;
-    }[];
-
-    status: string;
     tagline: string | null;
     title: string;
-    video: boolean;
     vote_average: number;
-    vote_count: number;
 
 
 };
@@ -77,31 +38,17 @@ export type MovieDetail = {
 export type credits = {
     id: number;
     cast: {
-        adult: boolean;
-        gender: number;
         id: number;
         known_for_department: string;
         name: string;
-        original_name: string;
-        popularity: number;
         profile_path: string;
-        cast_id: number;
-        character: string
-        credit_id: string;
-        order: number;
+        
     }[];
     crew: {
-        adult: boolean;
-        gender: number;
         id: number;
         known_for_department: string;
         name: string;
-        original_name: string;
-        popularity: number;
         profile_path: string;
-        credit_id: string;
-        department: string;
-        job: string;
 
     }[];
 
