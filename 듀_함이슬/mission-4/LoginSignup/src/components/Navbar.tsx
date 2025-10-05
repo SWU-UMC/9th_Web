76954e79ef0b71729ba6d1c3be3db26c4bcc8be1
dew102938 <eidnwq@gmail.com>
2025-10-05 15:59:51 +0900

    :sparkles: Feat: 4주차 미션 - Custom Hook, 로그인, 회원가입 기능 구현

---
+++ 듀_함이슬/mission-4/LoginSignup/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+const LINKS = [
+    {to: '/', label: '홈'},
+    {to: '/login', label: '로그인'},
+    {to: '/signup', label: '회원가입'},
+];
+
+export const Navbar = () => {
+    return (
+        <div className="flex gap-5 p-4 bg-gray-100">
+            {LINKS.map(({ to, label }) => (
+                <NavLink
+                    key={to}
+                    to={to}
+                    className={({ isActive }) => {
+                        return isActive ? "text-[green] font-bold" : "text-gray-500";
+                    }}>
+                    {label}
+                </NavLink>
+            ))}
+        </div>
+    );
+};
