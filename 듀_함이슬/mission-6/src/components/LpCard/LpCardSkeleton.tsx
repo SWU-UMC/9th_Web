3dab6293e93290eb2ff69720d1ddfa58d6b6e5ce
Dew <eidnwq@gmail.com>
2025-11-04 19:17:31 +0900

    :fire: Remove mission-6 from tracking

--- 듀_함이슬/mission-6/src/components/LpCard/LpCardSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-const LpCardSkeleton = () => {
-    return (
-        <div className="relative overflow-hidden shadow-lg animate-pulse w-44">
-            <div className={"bg-gray-300 aspect-square w-full h-48"} />
-        </div>
-    );
-};
-
-export default LpCardSkeleton;
